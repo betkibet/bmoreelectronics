d0ac305a3809d1df8f41bd5f011a8b926ef62dbc
betkibet <kibetnamanga@gmail.com>
2020-01-16 22:14:51 +0300

    Updated color styles of css

--- admin/_config/connect_db.php
+++ admin/_config/connect_db.php
@@ -8,6 +8,7 @@ $db_user = 'bmoreelectronics_bmore'; //Production Configuration
 //$db_user = 'root'; //Local Configuration
 $db_password = '12selldevice@34'; //Production Configuration
 //$db_password = ''; //Local Configuration
+//$db_name = 'buybackpowerswif_buyback';
 $db_name = 'bmoreelectronics_bmoreelectronics';
 
 $db = mysqli_connect($host,$db_user,$db_password,$db_name) or die('Unable to connect to the database');
