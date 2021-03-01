cdd295aa5c9f8c015730e7bc1c08d42f4bcf75ad
betkibet <kibetnamanga@gmail.com>
2021-03-01 23:33:41 +0300

    Included constants file

---
+++ admin/_config/constants.php
@@ -0,0 +1,82 @@
+<?php
+$searchbox_placeholder = "Search by device or model name...";
+
+$quote_request_popup_title = "Can’t Find Your Item?";
+$quote_request_popup_sub_title = "If you want to find out how much your item is worth, please fill out the form below and we will reply to your email within 24 hours. Remember, the more information you include about the item(s) you are trying to sell, the easier it is for us to make you an offer. Please fill out a good contact number and email address in case we need to ask you a few more questions.";
+$quote_request_popup_desc = "Quote Request";
+
+$review_form_stars_title = "How would you rate this business overall?";
+
+$imei_sn_check_tooltips_text = "Checking the ESN and IMEI numbers of the used phone you are trying to sell will help to ensure to your customers that you are selling a quality product. By checking the ESN and IMEI numbers, you will be able to see whether or not the phone has been stolen or is still under financing. This is just another way to establish trust and reliability between you and your customers.";
+
+$payment_method_bank_heading_text = "Bank Transfer";
+$payment_method_cheque_heading_text = "";
+$payment_method_paypal_heading_text = "PayPal";
+$payment_method_zelle_heading_text = "Zelle";
+$payment_method_venmo_heading_text = "Venmo";
+$payment_method_amazon_gcard_heading_text = "Amazon GCard";
+$payment_method_cash_heading_text = "Cash";
+$payment_method_cash_app_heading_text = "Cash App";
+$payment_method_apple_pay_heading_text = "Apple Pay";
+$payment_method_google_pay_heading_text = "Google Pay";
+$payment_method_coinbase_heading_text = "Coinbase";
+$payment_method_facebook_pay_heading_text = "Facebook Pay";
+
+$profile_payment_method_title = "Payment method";
+$profile_payment_method_act_name_place_holder_text = "Account Holder Name";
+$profile_payment_method_act_number_place_holder_text = "Account Number";
+$profile_payment_method_act_short_code_place_holder_text = "Short Code";
+$profile_payment_method_paypal_adr_place_holder_text = "PayPal Address";
+$profile_payment_method_paypal_adr_repeat_place_holder_text = "PayPal Confirm Address";
+$profile_payment_method_zelle_adr_place_holder_text = "Zelle Address";
+$profile_payment_method_venmo_adr_place_holder_text = "Venmo Address";
+$profile_payment_method_amazon_gcard_adr_place_holder_text = "Amazon GCard Address";
+$profile_payment_method_cash_name_place_holder_text = "Cash Name";
+$profile_payment_method_cash_phone_place_holder_text = "Mobile Phone number";
+$profile_payment_method_cash_app_adr_place_holder_text = "Cash App Address";
+$profile_payment_method_apple_pay_adr_place_holder_text = "Apple Pay Address";
+$profile_payment_method_google_pay_adr_place_holder_text = "Google Pay Address";
+$profile_payment_method_coinbase_adr_place_holder_text = "Coinbase Address";
+$profile_payment_method_facebook_pay_adr_place_holder_text = "Facebook Pay Address";
+
+$cart_payment_method_title = "Payment method";
+$cart_payment_method_act_name_place_holder_text = "Account Holder Name";
+$cart_payment_method_act_number_place_holder_text = "Account Number";
+$cart_payment_method_act_short_code_place_holder_text = "Short Code";
+$cart_payment_method_paypal_adr_place_holder_text = "yourpaypal@adress.com";
+$cart_payment_method_paypal_adr_repeat_place_holder_text = "Repeat yourpaypal@adress.com";
+$cart_payment_method_zelle_adr_place_holder_text = "Zelle Address";
+$cart_payment_method_venmo_adr_place_holder_text = "Venmo Address";
+$cart_payment_method_amazon_gcard_adr_place_holder_text = "Amazon GCard Address";
+$cart_payment_method_cash_name_place_holder_text = "Cash Name";
+$cart_payment_method_cash_phone_place_holder_text = "Mobile Phone number";
+$cart_payment_method_cash_app_adr_place_holder_text = "Cash App Address";
+$cart_payment_method_apple_pay_adr_place_holder_text = "Apple Pay Address";
+$cart_payment_method_google_pay_adr_place_holder_text = "Google Pay Address";
+$cart_payment_method_coinbase_adr_place_holder_text = "Coinbase Address";
+$cart_payment_method_facebook_pay_adr_place_holder_text = "Facebook Pay Address";
+
+$cart_shipping_method_title = "Shipping Method";
+$cart_select_shipping_method_title = "Select Shipping Method";
+$cart_terms_and_conditions_title = "Terms & Conditions";
+
+$cart_shipping_method_post_me_a_prepaid_label_title = "Send me a Box and Label";
+$cart_shipping_method_print_a_prepaid_label_title = "Print A Prepaid Label";
+$cart_shipping_method_use_my_own_courier_title = "I will ship and send you tracking";
+$cart_shipping_method_we_come_for_you_title = "schedule on demand pickup";
+$cart_shipping_method_store_title = "In store drop off";
+$cart_shipping_method_starbucks_title = "schedule a local meet up";
+
+$model_list_heading_title = "Choose you model";
+$model_list_heading_sub_title = "<span> to calculate the cost:</span>";
+
+$model_detail_any_condition_heading = "Choose if <b>ANY</b> of the following are true";
+$model_detail_all_condition_heading = "Choose if <b>ALL</b> of the following are true";
+
+$affiliate_shop_title = "Sell ​​your Apple product";
+$affiliate_shop_search_title = "Search By Device";
+
+$order_complete_mail_in_tooltips_admin = "Send me a Box and Label, Print A Prepaid Label, I will ship and send you tracking";
+$order_complete_drop_by_tooltips_admin = "Meet @ Phone Store";
+$order_complete_meet_up_tooltips_admin = "Meet @ Coffee Shop, We Come To You";
+?>
